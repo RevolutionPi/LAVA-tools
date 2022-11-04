c42d170c41587ee4e23c689ec27f25e81d791073
Frank Erdrich <f.erdrich@kunbus.com>
2022-11-04 08:44:37 +0100

    Initial commit of the deployment helper script.
    This script should be used by the LAVA worker together with the recovery
    deployment type of LAVA to bring a device under test into the correct
    state to be able to program a new image.
    
    Signed-off-by: Frank Erdrich <f.erdrich@kunbus.com>

---
+++ worker/tools/relaiscard.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+__number_to_onoff () {
+  [ "$1" -eq 0 ] && echo "off" || echo "on"
+}
+
+__send_cmd () {
+  echo "$3" | nc -w 5 -N "$1" "$2"
+  return $?
+}
+
+# parameters:
+#   $1 ip address of the relaiscard
+#   $2 port of the relaiscard
+#   $3 which relais to set as number (1 to max relais number)
+#   $4 value to set relais to as number (0, 1)
+rc_set_relais () {
+  if [[ $# != 4 ]]; then
+    echo "Error: wrong number of function parameters"
+    return 1
+  fi
+
+  __send_cmd "$1" "$2" "SR $3 $(__number_to_onoff "$4")" &>/dev/null
+
+  return $?
+}
+
+# parameters:
+#   $1 ip address of the relaiscard
+#   $2 port of the relaiscard
+#   $3 which gpio to set; as number (1 to max gpio number)
+#   $4 value to set relais to; as number (0, 1)
+rc_set_gpio () {
+  if [[ $# != 4 ]]; then
+    echo "Error: wrong number of function parameters"
+    return 1
+  fi
+
+  __send_cmd "$1" "$2" "SO $3 $(__number_to_onoff "$4")" &>/dev/null
+
+  return $?
+}
+
+# parameters:
+#   $1 ip address of the relaiscard
+#   $2 port of the relaiscard
+rc_get_status () {
+  if [[ $# != 2 ]]; then
+    echo "Error: wrong number of function parameters"
+    return 1
+  fi
+
+  __send_cmd "$1" "$2" "ST" &>/dev/null
+
+  return $?
+}
+
